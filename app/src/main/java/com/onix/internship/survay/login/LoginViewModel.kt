9150ff70cc70612b719fcaa68ed6e57db3f2b3b5
Oleksandr <glorikum@gmail.com>
2021-05-01 11:38:45 +0300

    Initial commit

---
+++ app/src/main/java/com/onix/internship/survay/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.onix.internship.survay.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/onix/internship/survay/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.onix.internship.survay.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
